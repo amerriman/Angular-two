57342c60c62a51244af3ab9558434de8a2244c56
amerriman <amm434@gmail.com>
2015-09-17 10:08:34 -0600

    completed first exercise

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,25 @@
 app.controller('myController', function($scope) {
   $scope.greeting = "Hello World!";
 });
+
+
+app.controller('DogsController', function($scope){
+  $scope.dogs = [
+    {
+      type: "Golden Retreiver",
+      friendly: "yes"
+    },
+    {
+      type: "Boxer",
+      friendly: "yes"
+    },
+    {
+      type: "Snoop",
+      friendly: "If there's gin and juice, then yes."
+    },
+  ];
+});
+
+app.controller('HomeController', function($scope){
+  $scope.message = "tacos"
+});
